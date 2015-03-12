8ddca6e22eb08f4c78981a47933a74965b39881e
Erik Slagter <erik@openpli.org>
2015-03-12 16:20:14 +0100

    Fix missing , in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 pkg = 'Exensions.HetWeer'
 setup (name = 'enigma2-plugin-extensions-hetweer',
 	version = '1.0',
-	description = '=--Weer App--=\nMade by DEG 2012~2015!'
+	description = '=--Weer App--=\nMade by DEG 2012~2015!',
 	packages = [pkg],
 	package_dir = {pkg: 'plugin'}
 )
