ed4640d1404fbc99f2b7ce7a9dbe25c23c003ca6
Erik Slagter <erik@openpli.org>
2015-03-13 12:29:06 +0100

    Corrected silly typo.
    This fixes the error:
    	Some plugins are not available: Exensions/HetWeer (No module named Exensions.HetWeer.plugin)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-pkg = 'Exensions.HetWeer'
+pkg = 'Extensions.HetWeer'
 setup (name = 'enigma2-plugin-extensions-hetweer',
 	version = '1.0',
 	description = '=--Weer App--=\nMade by DEG 2012~2015!',
