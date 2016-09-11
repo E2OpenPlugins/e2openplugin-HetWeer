56724005c7c4d884f71ae133929a1d248a80bc60
RobvanderDoes <rlvddoes@gmail.com>
2016-09-11 17:11:54 +0200

    Upgraded 'HetWeer' with all the work done by Caught. Thanks to him the plugin is fully functional again!
    Note 1: I kept the original name.
    Note 2: The Auto-update has been removed, as this is no longer required since the plugin is now being built from the sources.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 pkg = 'Extensions.HetWeer'
 setup (name = 'enigma2-plugin-extensions-hetweer',
-	version = '1.0',
-	description = '=--Weer App--=\nMade by DEG 2012~2015!',
+	version = '2.3',
+	description = '=--Weer App--=\nMade by Caught @http://forums.openpli.org',
 	packages = [pkg],
 	package_dir = {pkg: 'plugin'}
 )
